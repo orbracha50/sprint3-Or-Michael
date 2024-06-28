366bb8f958525e21a3caea6216c81b28b970de3d
Or Bracha <orbracha50@gmail.com>
2024-06-28 21:50:05 +0300

    Add or information to about page

--- pages/About.jsx
+++ pages/About.jsx
@@ -4,10 +4,14 @@ export function About() {
       <h1>About Us</h1>
 
       <div className="profile-card">
-        <img className="developer-image" src="" alt="or-image"></img>
+        <img className="developer-image" src="./assets/img/default-user-img.png" alt="or-image"></img>
         <div className="developer-info">
-        <h1>Information About Or:</h1>
-        <p> I'm the developer of the Keep App.......</p>
+          <h1>Information About Or:</h1>
+          <p>I'm the developer of the Keep app which is mimiced from Google
+            Keep and has a backend mimiced server-side using LocalStorage with
+            a bunch of demo data to show the user the Notes system.
+            If you're going to ask about the image yes this is totatlly me only because I'm the developer of the Keep App Currently,
+            a bunch of features are under development (Special Thanks to Risan for telling us this is a free bonus page 😁).</p>
         </div>
       </div>
       <div className="profile-card">
